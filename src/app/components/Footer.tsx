553dfbc3957dee8b259dba496423780530a4a1ff
Rutger van Rees <r.vanrees@icloud.com>
2023-12-06 15:53:50 +0100

    Added Footer
    Added Footer for the first deployment on Vercel.

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,41 @@
+import packageJson from "../../../package.json";
+
+const Footer = () => {
+  return (
+    <div className="mx-auto max-w-2xl lg:max-w-4xl px-4 sm:px-6 lg:px-8 py-4">
+      <div className="flex flex-row justify-between border-t border-black/10 py-2">
+        <div className="mt-1.5">
+          <p className="text-xs">
+            <a
+              href="https://fraight.online"
+              className="font-bold text-black/50 hover:text-black transition mr-1"
+            >
+              Fraight
+            </a>
+            <span className="font-mono">v{packageJson.version}</span>
+          </p>
+        </div>
+        <div className="hidden lg:block p-x-4"></div>
+        <div className="flex flex-row mt-0.5">
+          <div>
+            <a href="https://github.com/fraightonline" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="ml-2 fill-black/50 hover:fill-black transition"
+              >
+                <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Footer from "./components/Footer";
+
 export default function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold">Fraight</h1>
-      <h2 className="text-lg">AI driven Freight Technologies.</h2>
-    </div>
+    <>
+      <Footer />
+    </>
   );
 }
